24246ac2516994cd10f47319628c6cf8378ff7c5
RoshniSingh2068 <roshnisingh1228@gmail.com>
2024-08-26 11:36:18 +0530

    Create the else statement c++.c

---
+++ the else statement c++.c
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int time = 20;
+    if(time< 18){
+        cout<<"it is good day";
+
+    } else {
+        cout << "it is not good as you know ";
+
+    }
+    return 0;
+
+}
